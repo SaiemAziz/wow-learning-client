66abe0c5a00a336c37d32f4d1085f36a7fd6b9a6
SaiemAziz <sayemazizchy@gmail.com>
2022-10-25 17:32:34 +0600

    subjects div created

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useLocation } from 'react-router-dom';
 
 const Category = () => {
 
     let subjects = useLoaderData();
     console.log(subjects);
-
+    let cat = useLocation();
+    cat = (cat.pathname.replace('/courses/', ''))
+    cat = cat.charAt(0).toUpperCase() + cat.slice(1)
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-5 p-5'>
+            <p className='mt-5 text-5xl font-bold italic'>Category: {cat}</p>
+            <p className='mb-5 text-5xl font-bold italic'>Total: {subjects.length}</p>
+            {
+                subjects.map(sub=>{
+                    return <div className='card glass p-5 shadow-xl'>
+                        <img src={sub.pic} alt="" />
+                        <p className='text-3xl'>Name: {sub.name}</p>
+                    </div>
+                })
+            }
         </div>
     );
 }; 
